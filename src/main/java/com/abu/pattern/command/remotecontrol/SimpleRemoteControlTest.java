643d6ab4d623ab8fce7db03f9dad660a44c479f9
pure <502964379@qq.com>
2018-01-12 17:51:00 +0800

    1、命令模式DAY1

--- src/main/java/com/abu/pattern/command/remotecontrol/SimpleRemoteControlTest.java
+++ src/main/java/com/abu/pattern/command/remotecontrol/SimpleRemoteControlTest.java
@@ -7,10 +7,13 @@ public class SimpleRemoteControlTest {
 
     public static void main(String[] args) {
         //初始化遥控器
+        //请求发送者，commmand sender
         SimpleRemoteControl simpleRemoteControl = new SimpleRemoteControl();
 
         //初始化灯，以及控制灯的命令
+        //请求接受者 commmand reciver
         Light light = new Light("Bedroom Light");
+        //command
         Command lightOnCommand = new LightOnCommand(light);
 
         //将初始化完毕的命令对象，设置进遥控器。
